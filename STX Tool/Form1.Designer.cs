9382999d618506a7aeeaaa32770ef58d9d127889
Reymar Guerrero <reymar.guerrero@outlook.com>
2015-04-04 19:06:45 -0700

    First Commit

---
+++ STX Tool/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace STX_Tool
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(19, 60);
+            this.button1.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button1.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 121);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(14, 25);
+            this.tabControl1.MaximumSize = new System.Drawing.Size(738, 374);
+            this.tabControl1.MinimumSize = new System.Drawing.Size(738, 374);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(738, 374);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.button14);
+            this.tabPage1.Controls.Add(this.button13);
+            this.tabPage1.Controls.Add(this.button12);
+            this.tabPage1.Controls.Add(this.button11);
+            this.tabPage1.Controls.Add(this.button10);
+            this.tabPage1.Controls.Add(this.button9);
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(730, 348);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(613, 204);
+            this.button14.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button14.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(93, 121);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "Service Monitor";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(514, 204);
+            this.button13.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button13.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(93, 121);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "Registry Editor";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(415, 204);
+            this.button12.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button12.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(93, 121);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "Event Viewer";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(316, 204);
+            this.button11.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button11.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(93, 121);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Device Manager";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(217, 204);
+            this.button10.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button10.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(93, 121);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Disk Defragment";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(118, 204);
+            this.button9.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button9.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(93, 121);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Disck Cleanup";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(19, 204);
+            this.button8.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button8.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(93, 121);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Disk Manager";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(613, 60);
+            this.button7.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button7.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(93, 121);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Control Panel";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(514, 60);
+            this.button6.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button6.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 121);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "System Restore";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(415, 60);
+            this.button5.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button5.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 121);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "System Monitor";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(316, 60);
+            this.button4.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button4.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 121);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "System Configure";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(217, 60);
+            this.button3.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button3.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 121);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "System Manager";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 60);
+            this.button2.MaximumSize = new System.Drawing.Size(93, 121);
+            this.button2.MinimumSize = new System.Drawing.Size(93, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 121);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "System Properties ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(730, 348);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Cleaner";
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(730, 348);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Programs and Features";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(730, 348);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Startup Manager";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(730, 348);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Process Manager";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Location = new System.Drawing.Point(-1, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(767, 404);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Location = new System.Drawing.Point(1, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(765, 57);
+            this.panel2.TabIndex = 3;
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(762, 480);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Test";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "SUPPORTrix Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ STX Tool/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace STX_Tool
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("dxdiag");
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("sysdm.cpl");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("taskmgr");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Control");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("rstrui.exe");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("msconfig");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("diskmgmt.msc");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cleanmgr.exe");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Defrag.exe");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("devmgmt.msc");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("eventvwr");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("regedit");
+            }
+            catch
+            {
+                Console.WriteLine("HERE");
+            }
+            
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Services.msc");
+        }
+    }
+}
